f9f44476aa6d1f37928b52a5be79d5d9d81df7f1
drakeet <drakeet.me@gmail.com>
2015-08-02 17:29:41 +0800

    add and init main search bar item view;

--- app/src/main/kotlin/me.drakeet.smartisannote/adapter/NoteListAdapter.kt
+++ app/src/main/kotlin/me.drakeet.smartisannote/adapter/NoteListAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import me.drakeet.smartisannote.R
 import me.drakeet.smartisannote.api.OnNoteTouchListener
 import me.drakeet.smartisannote.model.Note
@@ -16,31 +17,62 @@ import me.drakeet.smartisannote.widget.ListNotesItemLayout
 public class NoteListAdapter(val mContext: Context, val mList: List<Note>) : RecyclerView.Adapter<NoteListAdapter.ViewHolder>() {
 
     var mOnNoteTouchListener: OnNoteTouchListener? = null
+    private val TYPE_SEARCH = 0
+    private val TYPE_NOTE = 1
 
-    override fun onCreateViewHolder(parent: ViewGroup, i: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_note, parent, false)
-        return ViewHolder(v)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view : View? = null
+        when (viewType) {
+            TYPE_SEARCH -> {
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_header_search_view, parent, false)
+            }
+            TYPE_NOTE -> {
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_note, parent, false)
+            }
+        }
+        // 这里的 ViewHolder 的 Type 可能还没初始化，所以得手动传入... 使用 viewHolder.getItemViewType() 可能回出问题
+        return ViewHolder(view!!, viewType)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val note = mList.get(position)
-        viewHolder.layoutView!!.setNote(note)
+        when (viewHolder.getItemViewType()) {
+            TYPE_SEARCH -> {
+                // TODO
+            }
+            TYPE_NOTE -> {
+                val notePosition = position - 1
+                val note = mList.get(notePosition)
+                viewHolder.layoutView!!.setNote(note)
+            }
+        }
+    }
+
+    override public fun getItemViewType(position: Int): Int {
+        return if (position == 0) TYPE_SEARCH else TYPE_NOTE
     }
 
     override fun getItemCount(): Int {
-        return mList.size()
+        return mList.size() + 1
     }
 
     public fun setOnMeizhiTouchListener(onNoteTouchListener: OnNoteTouchListener) {
         this.mOnNoteTouchListener = onNoteTouchListener
     }
 
-    public inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    public inner class ViewHolder(itemView : View, viewType : Int) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
         var layoutView: ListNotesItemLayout? = null
+        var searchView: EditText? = null
 
         init {
-            layoutView = itemView as ListNotesItemLayout
+            when (viewType) {
+                TYPE_NOTE -> {
+                    layoutView = itemView as? ListNotesItemLayout
+                }
+                TYPE_SEARCH -> {
+                    searchView = itemView.findViewById(R.id.edittext_search) as? EditText
+                }
+            }
         }
 
         override fun onClick(v: View) {
